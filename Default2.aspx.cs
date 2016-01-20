2e628a0101d854d9ac5d94a411d537579b5f58d2
lixiaolei19821218 <735534185@qq.com>
2016-01-20 21:41:25 +0800

    UK Mail QA

---
+++ Default2.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ServiceReferenceUKMailQA;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        LoginWebRequest request = new LoginWebRequest();
+        request.Username = "735534185@qq.com";
+        request.Password = "UKMail123";
+
+        UKMAuthenticationServiceClient auth = new UKMAuthenticationServiceClient();
+        try
+        {
+            UKMLoginResponse response = auth.Login(request);
+            if (response.Result == UKMResultState.Successful)
+            {
+                message.InnerText = UKMResultState.Successful.ToString();
+            }
+            else if (response.Result == UKMResultState.Failed)
+            {
+                message.InnerText = UKMResultState.Failed.ToString();
+            }
+            else
+            {
+                message.InnerText = UKMResultState.Unknown.ToString();
+            }
+        }
+        catch (Exception ex)
+        {
+            message.InnerText = ex.Message;
+        }
+    }
+}
