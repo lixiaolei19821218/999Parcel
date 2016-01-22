725e95dc84552ff518589feed54c4810fa91703f
lixiaolei19821218 <735534185@qq.com>
2016-01-22 16:33:46 +0800

    collection

--- Default2.aspx.cs
+++ Default2.aspx.cs
@@ -5,35 +5,47 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using ServiceReferenceUKMailQA;
+using UKMCollectionService;
 
 public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        LoginWebRequest request = new LoginWebRequest();
-        request.Username = "735534185@qq.com";
-        request.Password = "UKMail123";
-
+        LoginWebRequest loginRequest = new LoginWebRequest();
+        loginRequest.Username = "735534185@qq.com";
+        loginRequest.Password = "UKMail123";
+        UKMLoginResponse loginResponse = null;
         UKMAuthenticationServiceClient auth = new UKMAuthenticationServiceClient();
         try
         {
-            UKMLoginResponse response = auth.Login(request);
-            if (response.Result == UKMResultState.Successful)
+            loginResponse = auth.Login(loginRequest);
+            message.InnerText = loginResponse.Result.ToString();      
+        }
+        catch (Exception ex)
+        {
+            message.InnerText = ex.Message;
+        }
+        UKMCollectionServiceClient collectionService = new UKMCollectionServiceClient();
+        AddCollectionWebRequest collectionRequest = new AddCollectionWebRequest();
+        collectionRequest.AccountNumber = "S900118";
+        collectionRequest.AuthenticationToken = loginResponse.AuthenticationToken;
+        collectionRequest.ClosedForLunch = false;
+        collectionRequest.EarliestTime = DateTime.Today.AddDays(3).AddHours(11);
+        collectionRequest.LatestTime = DateTime.Today.AddDays(3).AddHours(17);
+        collectionRequest.RequestedCollectionDate = DateTime.Today.AddDays(3).AddHours(17);
+        collectionRequest.SpecialInstructions = "Test";
+        collectionRequest.Username = "735534185@qq.com";
+
+        UKMAddCollectionWebResponse colloctionResponse = null;
+        try
+        {
+            colloctionResponse = collectionService.BookCollection(collectionRequest);
+            foreach (UKMCollectionService.UKMWebError error in colloctionResponse.Errors)
             {
-                message.InnerText = UKMResultState.Successful.ToString();
+                message.InnerText += error.Description + "\n\r";
             }
-            else if (response.Result == UKMResultState.Failed)
-            {
-                message.InnerText = UKMResultState.Failed.ToString();
-            }
-            else
-            {
-                message.InnerText = UKMResultState.Unknown.ToString();
-            }
-            UKMCollectionService.UKMCollectionServiceClient c = new UKMCollectionService.UKMCollectionServiceClient();
-            UKMCollectionService.AddCollectionWebRequest r = new UKMCollectionService.AddCollectionWebRequest();
-            
-            c.BookCollection(r);
+            //message.InnerText = colloctionResponse.Result.ToString();
+            //message.InnerText = collectionRequest.AuthenticationToken;
         }
         catch (Exception ex)
         {
