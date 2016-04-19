721015efa04ce6682c73c092bda15a2ef0e965e9
lixiaolei19821218 <735534185@qq.com>
2016-04-19 16:59:39 +0800

    modify new css

---
+++ MasterPage_Old.master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            if (Request.IsAuthenticated)
+            {
+                loginDiv.Visible = false;
+                welcomeDiv.Visible = true;
+            }
+            else
+            {
+                loginDiv.Visible = true;
+                welcomeDiv.Visible = false;
+            }
+        }       
+    }
+
+    protected string GetGreeting()
+    {
+        return Context.User.Identity.Name;
+    }
+}
