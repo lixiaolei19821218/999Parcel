ba4a892f8f85cb9900eb76b5efe0e4fe9eafbef2
lixiaolei19821218 <735534185@qq.com>
2016-08-01 22:59:15 +0800

    搜索

--- App_Code/HttpHelper.cs
+++ App_Code/HttpHelper.cs
@@ -16,47 +16,42 @@ public static class HttpHelper
 
     public static string HttpPost(string Url, string postDataStr)
     {
-
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
-
-        request.Method = "POST";
-
-        request.ContentType = "application/x-www-form-urlencoded";
-
-        request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
-
-        //request.CookieContainer = cookie;
-
-        Stream myRequestStream = request.GetRequestStream();
-
-        StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
-
-        myStreamWriter.Write(postDataStr);
-
-        myStreamWriter.Close();
-
-
-
-        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-
-
-        //response.Cookies = cookie.GetCookies(response.ResponseUri);
-
-        Stream myResponseStream = response.GetResponseStream();
-
-        StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
-
-        string retString = myStreamReader.ReadToEnd();
-
-        myStreamReader.Close();
-
-        myResponseStream.Close();
-
-
-
-        return retString;
-
+        Stream outstream = null;
+        Stream instream = null;
+        StreamReader sr = null;
+        HttpWebResponse response = null;
+        HttpWebRequest request = null;
+        Encoding encoding = System.Text.Encoding.GetEncoding("UTF-8");
+        byte[] data = encoding.GetBytes(postData);
+        // 准备请求...  
+        try
+        {
+            // 设置参数  
+            request = WebRequest.Create(posturl) as HttpWebRequest;
+            CookieContainer cookieContainer = new CookieContainer();
+            request.CookieContainer = cookieContainer;
+            request.AllowAutoRedirect = true;
+            request.Method = "POST";
+            request.ContentType = "text/json";
+            request.ContentLength = data.Length;
+            outstream = request.GetRequestStream();
+            outstream.Write(data, 0, data.Length);
+            outstream.Close();
+            //发送请求并获取相应回应数据  
+            response = request.GetResponse() as HttpWebResponse;
+            //直到request.GetResponse()程序才开始向目标网页发送Post请求  
+            instream = response.GetResponseStream();
+            sr = new StreamReader(instream, encoding);
+            //返回结果网页（html）代码  
+            string content = sr.ReadToEnd();
+            string err = string.Empty;
+            return content;
+        }
+        catch (Exception ex)
+        {
+            string err = ex.Message;
+            return string.Empty;
+        }
     }
 
 
